1fd2d96051de8e21593bc60a45d1ed257922107d
Angus Grieve-Smith <grvsmth@panix.com>
2022-01-13 00:56:45 -0500

    Added forearm

--- js/humanoid.js
+++ js/humanoid.js
@@ -12,23 +12,29 @@ renderer.setSize( threedDiv.offsetWidth, threedDiv.offsetHeight );
 threedDiv.appendChild( renderer.domElement );
 
 const humanoid = new THREE.Group();
+const bodyMaterial = new THREE.MeshBasicMaterial( { color: 0x505050 } );
+const skinMaterial = new THREE.MeshBasicMaterial( {color: 0xFF9A66} );
+const skinMaterial2 = new THREE.MeshBasicMaterial( {color: 0xFF9A66} );
 
 const bodyGeometry = new THREE.BoxGeometry(3.684, 2.508, 1.08);
-const bodyMaterial = new THREE.MeshBasicMaterial( { color: 0x505050 } );
 const bodyMesh = new THREE.Mesh( bodyGeometry, bodyMaterial );
-
 humanoid.add(bodyMesh);
 
+const forearmGeometry = new THREE.CylinderGeometry(0.288, 0.288, 1.92);
+const forearmMesh = new THREE.Mesh(forearmGeometry, skinMaterial2);
+forearmMesh.position.set(0, -0.96, 0);
+
+const elbow = new THREE.Group();
+elbow.add(forearmMesh);
+elbow.position.set(0, -2.28, 0);
+
+const upperArmGeometry = new THREE.CylinderGeometry(0.288, 0.288, 2.28);
+const upperArmMesh = new THREE.Mesh(upperArmGeometry, skinMaterial);
+upperArmMesh.position.set(0, -1.14, 0);
+
 const shoulder = new THREE.Group();
-
-const armGeometry = new THREE.CylinderGeometry(0.288, 0.288, 2.28);
-const skinMaterial = new THREE.MeshBasicMaterial( {color: 0xFF9A66} );
-const armMesh = new THREE.Mesh(armGeometry, skinMaterial);
-
-armMesh.position.set(0, -1.14, 0);
-
-shoulder.add(armMesh);
-
+shoulder.add(upperArmMesh);
+shoulder.add(elbow);
 shoulder.position.set(-1.8, 1.14, 0);
 
 humanoid.add(shoulder);
@@ -54,7 +60,9 @@ function animate(timestamp) {
 
     if (previousTimestamp !== timestamp) {
 	shoulder.rotation.x -= 0.01;
+	elbow.rotation.x += 0.005;
 	shoulder.rotation.y -= 0.01;
+	elbow.rotation.y -= 0.05;
 	renderer.render( scene, camera );
     }
 
