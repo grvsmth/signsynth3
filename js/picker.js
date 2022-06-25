cd49dd0795ae64b279726af24ec6b0570694424f
Angus Grieve-Smith <grvsmth@panix.com>
2022-06-25 13:45:33 -0400

    Add keyboard input

--- js/picker.js
+++ js/picker.js
@@ -4,6 +4,23 @@ const param = {
     "orientation-letter": "orientation"
 };
 
+const ignoreKeys = ["Alt",
+                    "ArrowDown",
+                    "ArrowLeft",
+                    "ArrowRight",
+                    "ArrowUp",
+                    "CapsLock",
+                    "Control",
+                    "End",
+                    "Escape",
+                    "Home",
+                    "Insert",
+                    "Meta",
+                    "PageDown",
+                    "PageUp",
+                    "Shift",
+                    "Tab"];
+
 const exports = {
     "append": function(addition) {
         document.querySelector("#ascsto-output").append(addition);
@@ -32,9 +49,7 @@ const exports = {
             console.log("Can't create clipboardItem!", error);
         }
 
-        navigator.clipboard.write([htmlItem]).then(() => {
-            console.log("Successfully copied HTML");
-        }, (error) => {
+        navigator.clipboard.write([htmlItem]).catch((error) => {
             console.log("Failed to copy HTML", error);
         });
     },
@@ -51,27 +66,95 @@ exports.spaceListener = function() {
     exports.append(" ");
 };
 
-exports.letterListener = function(event) {
+exports.addLetter = function(letter) {
     const charFormatElement = document.forms["format-form"]
           .elements["char-format"];
 
     if (charFormatElement.value === "superscript") {
-        exports.appendElement("sup", event.target.innerText);
+        exports.appendElement("sup", letter);
         charFormatElement.value = "none";
         return;
     }
 
     if (charFormatElement.value === "subscript") {
-        exports.appendElement("sub", event.target.innerText);
+        exports.appendElement("sub", letter);
         charFormatElement.value = "none";
         return;
     }
 
-    exports.append(event.target.innerText);
+    exports.append(letter);
+};
+
+exports.letterListener = function(event) {
+    if (event.type === "click") {
+        exports.addLetter(event.target.innerText);
+        return;
+    }
+
+    if (ignoreKeys.includes(event.key)) {
+        return;
+    }
+
+    if (event.key.length > 1) {
+        if (event.key.substring(0,1) === "F") {
+            return;
+        }
+
+        if (event.key === "Backspace") {
+            exports.backspaceListener();
+            return;
+        }
+    }
+
+    if (event.ctrlKey) {
+        if (event.shiftKey) {
+            if (event.key === "x") {
+                exports.copyHtmlListener();
+                exports.clearListener();
+                return;
+            }
+
+            if (event.key === "c") {
+                exports.copyHtmlListener();
+                return;
+            }
+
+            if (event.key === "+") {
+                event.preventDefault();
+                const charFormatElement = document.forms["format-form"]
+                      .elements["char-format"];
+                charFormatElement.value = "superscript";
+                return;
+            }
+        }
+
+        if (event.key === "x") {
+            exports.copyListener();
+            exports.clearListener();
+            return;
+        }
+
+        if (event.key === "c") {
+            exports.copyListener();
+            return;
+        }
+
+        if (event.key === "=") {
+            event.preventDefault();
+            const charFormatElement = document.forms["format-form"]
+                  .elements["char-format"];
+            charFormatElement.value = "subscript";
+            return;
+        }
+
+        // Paste is a big deal because we have to sanitize it
+    }
+
+    exports.addLetter(event.key);
 };
 
 exports.addListener = function(pickerLetter) {
     pickerLetter.addEventListener("click", exports.letterListener);
-}
+};
 
 export default exports;
